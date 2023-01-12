77b201e73df2f8560ac71dc847f09a83b1ff8ef9
rmbodegalibre <rmbodegalibre@gmail.com>
2023-01-12 12:37:04 -0500

    task4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-echo '## Production Instructions
 
-["PRESENT"]
-* The production website is hosted in an Ubuntu 18.04 Docker container
-* The applications "GoHugo" and "Make" are installed with `apt-get update && apt-get install -y hugo make`.
-* When running the command `make build`, there is a bunch of errors which end with the following lines:
+# Fixing install hugo error 
+apt-get update && apt-get install -y curl
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt-get install -y ./hugo.deb
+rm hugo.deb
+apt-get install -y make
 
-["FAILS"]
-Error: Error building site: logged 15 error(s)'
-echo "Makefile:2: recipe for target 'build' failed
-make: *** [build] Error 255"
+# Running hugo
+make build
