b7b35daae396acaa4a513429cb23fd558072950b
rmbodegalibre <rmbodegalibre@gmail.com>
2023-01-12 09:09:11 -0500

    task 4

---
+++ module1_task4/content/posts/welcome.md
@@ -0,0 +1,6 @@
+---
+title: "Welcome to Awesome Inc."
+date: 2023-01-11T11:21:10-05:00
+draft: true
+---
+
---
+++ module1_task4/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo '## Production Instructions
+
+["PRESENT"]
+* The production website is hosted in an Ubuntu 18.04 Docker container
+* The applications "GoHugo" and "Make" are installed with `apt-get update && apt-get install -y hugo make`.
+* When running the command `make build`, there is a bunch of errors which end with the following lines:
+
+["FAILS"]
+Error: Error building site: logged 15 error(s)'
+echo "Makefile:2: recipe for target 'build' failed
+make: *** [build] Error 255"
