5bbff495179e64982b49d0d6fbbd0e12433231ff
rmbodegalibre <rmbodegalibre@gmail.com>
2023-01-12 16:18:12 -0500

    task_4

---
+++ module1_task4/content/posts/welcome.md
@@ -0,0 +1,5 @@
+---
+title: "Welcome to Awesome Inc."
+date: 2023-01-12T16:13:19-05:00
+draft: true
+---
---
+++ module1_task4/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Fixing install hugo error 
+apt-get update && apt-get install -y curl
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
+apt-get install -y ./hugo.deb
+rm hugo.deb
+apt-get install -y make
+
+# Running hugo
+make build
