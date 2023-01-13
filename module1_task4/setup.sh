cbec37ef511dc8f15a648871bc85e84765b9f614
rmbodegalibre <rmbodegalibre@gmail.com>
2023-01-12 19:14:31 -0500

    setup.sh_05

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -5,7 +5,7 @@ apt-get update && apt-get install -y curl
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
 apt-get install -y ./hugo.deb
 rm hugo.deb
-apt-get install -y make
+apt-get install -y
 
 # Running hugo
 make build
