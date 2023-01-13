5d2773f99f379363a6acbc6af821907642a303a1
rmbodegalibre <rmbodegalibre@gmail.com>
2023-01-13 06:37:58 -0500

    setup.sh_06

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -2,10 +2,10 @@
 
 # Fixing install hugo error 
 apt-get update && apt-get install -y curl
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb -o hugo.deb
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 apt-get install -y ./hugo.deb
 rm hugo.deb
-apt-get install -y
+apt-get install -y make
 
 # Running hugo
 make build
