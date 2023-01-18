0ef86281e9938d8f85f4bcda8f8d38dbbb0e37a5
rmbodegalibre <rmbodegalibre@gmail.com>
2023-01-17 21:19:47 -0500

    Updated Makefile & setup.sh

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fixing install hugo error
-apt-get update && apt-get install -y make wget
-wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
-dpkg -i hugo_extended_0.109.0_linux-amd64.deb
-rm -f hugo_extended_0.109.0_linux-amd64.deb
+apt-get update && apt-get install -y curl make
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo_extended.deb
+apt-get install -y ./hugo_extended.deb
+rm hugo_extended.deb
