f824b694ddc330858eaf67373f4e35c6da158093
rmbodegalibre <rmbodegalibre@gmail.com>
2023-01-29 07:44:33 -0500

    main_test updated

--- module2_task3/main_test.go
+++ module2_task3/main_test.go
@@ -1,77 +1,82 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"net/url"
 	"testing"
 )
 
-func Test_HelloHandler(t *testing.T) {
-	if !testing.Short() {
-		t.Skip("Flag `-short` absent: skipping Unit Tests.")
+func Test_server(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Flag `-short` provided: skipping Integration Tests.")
 	}
 
 	tests := []struct {
 		name         string
-		queryString  string
+		URI          string
 		responseCode int
 		body         string
 	}{
 		{
-			name:         "Grace Hopper",
-			queryString:  "name=Grace Hopper",
-			responseCode: 200,
-			body:         "Hello Grace Hopper!",
+			name:         "Home page",
+			URI:          "",
+			responseCode: 404,
+			body:         "404 page not found\n",
 		},
 		{
-			name:         "Rosalind Franklin",
-			queryString:  "name=Rosalind Franklin",
+			name:         "Name Holberton",
+			URI:          "/hello?name=Holberton",
 			responseCode: 200,
-			body:         "Hello Rosalind Franklin!",
+			body:         "Hello Holberton!",
 		},
 		{
-			name:         "Without a name",
-			queryString:  "",
+			name:         "Health handler",
+			URI:          "/health",
+			responseCode: 200,
+			body:         "ALIVE",
+		},
+		{
+			name:         "Empty name",
+			URI:          "/hello",
 			responseCode: 200,
 			body:         "Hello there!",
 		},
 		{
 			name:         "With two name parameters",
-			queryString:  "name=Jose&name=Ricardo",
+			URI:          "/hello?name=Alejo&name=Luiyi",
 			responseCode: 200,
-			body:         "Hello Richie!",
+			body:         "Hello Luiyi!",
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			ts := httptest.NewServer(setupRouter())
+			defer ts.Close()
 
-			// Build an HTTP request for the current test `tt`
-			req, err := http.NewRequest("GET", "/hello", nil)
+			res, err := http.Get(ts.URL + tt.URI)
 			if err != nil {
 				t.Fatal(err)
 			}
 
-			queryParams, _ := url.ParseQuery(tt.queryString)
-			req.URL.RawQuery = queryParams.Encode()
-
-			// Initialize a new httptest serve with the function under test
-			rr := httptest.NewRecorder()
-			handler := http.HandlerFunc(HelloHandler)
-			handler.ServeHTTP(rr, req)
-
 			// Check that the status code is what you expect.
 			expectedCode := tt.responseCode
-			gotCode := rr.Code
+			gotCode := res.StatusCode
 			if gotCode != expectedCode {
-				t.Errorf("handler returned wrong status code: got %v want %v", gotCode, expectedCode)
+				t.Errorf("handler returned wrong status code: got %q want %q", gotCode, expectedCode)
 			}
 
 			// Check that the response body is what you expect.
 			expectedBody := tt.body
-			gotBody := rr.Body.String()
+			bodyBytes, err := ioutil.ReadAll(res.Body)
+			res.Body.Close()
+			if err != nil {
+				t.Fatal(err)
+			}
+			gotBody := string(bodyBytes)
 			if gotBody != expectedBody {
-				t.Errorf("handler returned unexpected body: got %v want %v", gotBody, expectedBody)
+				t.Errorf("handler returned unexpected body: got %q want %q", gotBody, expectedBody)
 			}
 		})
 	}
